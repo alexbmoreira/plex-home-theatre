1f74c009ca5c4fcabba9a1e47a3e6cfb1fad398f
Alex Moreira <alexbmoreira@gmail.com>
2024-08-21 19:06:33 -0400

    search bar

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -5,6 +5,16 @@ import { HomeState } from './state';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
+const Spinner = () => {
+  return (
+    <div className='flex justify-center items-center gap-2 mt-16'>
+      <div className='w-4 h-4 rounded-full bg-amethyst animate-bounce'></div>
+      <div className='w-4 h-4 rounded-full bg-amethyst animate-bounce [animation-delay:-.3s]'></div>
+      <div className='w-4 h-4 rounded-full bg-amethyst animate-bounce [animation-delay:-.5s]'></div>
+    </div>
+  )
+};
+
 const Poster = observer(({movie}) => {
   return (
     <div key={movie.guid} className='cursor-pointer w-full rounded bg-slate hover:bg-amethyst active:bg-amethyst-active'>
@@ -19,20 +29,23 @@ const Poster = observer(({movie}) => {
 });
 
 const Home = observer(({uiState}) => {
-  const {movies} = uiState;
+  const {movies, loadingMovies} = uiState;
 
   return (
     <div className='py-16 px-64'>
       <div className='font-serif text-3xl mb-4 text-center'>{'Select a Movie'}</div>
       <input
         type='search'
-        className='w-full'
+        className='w-full mb-4 p-2 bg-slate rounded outline-none'
         placeholder={'Search...'}
         onChange={_.debounce(e => uiState.updateSearch(e.target.value), 300)}
       />
-      <div className='grid gap-6 grid-cols-4'>
-        {movies.map((movie) => <Poster movie={movie}/>)}
-      </div>
+      {loadingMovies ?
+        <Spinner/> :
+        <div className='grid gap-6 grid-cols-4'>
+          {movies.map((movie) => <Poster movie={movie}/>)}
+        </div>
+      }
     </div>
   );
 });
--- client/src/pages/home/state/HomeState.js
+++ client/src/pages/home/state/HomeState.js
@@ -1,26 +1,25 @@
 import { makeObservable, observable, action } from 'mobx';
 import { fetchData } from '../../../api/api.service'
-import _ from 'lodash';
 
 class HomeState {
   movies = [];
   search = '';
-  fetchingMovies = true;
+  loadingMovies = true;
 
   constructor() {
     makeObservable(this, {
       movies: observable,
       search: observable,
-      fetchingMovies: observable,
+      loadingMovies: observable,
       load: action
     });
   }
 
   async load() {
-    this.fetchingMovies = true;
+    this.loadingMovies = true;
     const movies = await fetchData(`/api/movies?search=${this.search}`);
     this.movies = movies;
-    this.fetchingMovies = false;
+    this.loadingMovies = false;
   }
 
   async updateSearch(value) {
