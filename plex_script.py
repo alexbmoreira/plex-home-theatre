d488f440e5448228c72f0ca36b0e62b52e9c6eda
Alex Moreira <alexbmoreira@gmail.com>
2024-08-21 16:54:48 -0400

    wip

--- plex_script.py
+++ plex_script.py
@@ -2,28 +2,51 @@ from plexapi.server import PlexServer
 from dotenv import load_dotenv
 import os
 import subprocess
+import pychromecast
+from pychromecast.controllers.plex import PlexController
+import zeroconf
+from time import sleep
 
 load_dotenv()
 
 baseurl = os.environ.get('PLEX_URL')
 token = os.environ.get('PLEX_TOKEN')
 client = os.environ.get('PLEX_CLIENT')
+chromecast_name = os.environ.get('CHROMECAST_NAME')
+
+def find_movie_on_plex(title='12 Angry Men'):
+    plex_server = PlexServer(baseurl, token)
+    movies = plex_server.library.section('Movies')
+
+    return movies.get(title)
+
+def launch_plex_on_chromecast():
+    zconf = zeroconf.Zeroconf()
+    browser = pychromecast.CastBrowser(pychromecast.SimpleCastListener(lambda uuid, service: print(browser.devices[uuid].friendly_name)), zconf)
+    browser.start_discovery()
+    pychromecast.discovery.stop_discovery(browser)
+
+    chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[chromecast_name])
+
+    if not chromecasts:
+        print("No Chromecast devices found.")
+        return
+
+    cast = chromecasts[0]
+    plex_controller = PlexController()
+    cast.register_handler(plex_controller)
+
+    cast.wait()
+
+    plex_controller.block_until_playing(find_movie_on_plex())
 
 def turn_on_projector():
     try:
-        # subprocess.run(["echo", '"on 0"', "|", "cec-client", "-s", "-d", "1"], check=True)
         subprocess.run(["cec-client", "-s", "-d", "1"], input="on 0\n", text=True, check=True)
+        sleep(20)
+        print("Turned on projector")
     except subprocess.CalledProcessError as e:
         print(f"Failed to turn on projector: {e}")
 
 turn_on_projector()
-
-# plex = PlexServer(baseurl, token)
-# client = next(client for client in plex.clients() if client.title == client)
-# movies = plex.library.section('Movies')
-
-
-# for movie in movies.all():
-#     print(movie.guids[0].id)
-
-# response = client.playMedia(movie)
+launch_plex_on_chromecast()
